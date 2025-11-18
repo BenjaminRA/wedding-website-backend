4dc49280d2f190eeead333b2edb3397794d22567
Benjamin Rodriguez <benjamin.gra572@gmail.com>
2025-11-17 23:48:54 -0300

    fix rotation

--- config/plugins.ts
+++ config/plugins.ts
@@ -19,4 +19,27 @@ export default ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000,
+        },
+      },
+      sizeLimit: 250 * 1024 * 1024, // 250MB
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64,
+      },
+      // Fix image rotation by auto-rotating based on EXIF orientation
+      // and then removing the EXIF orientation tag
+      sharp: {
+        withMetadata: false, // Remove EXIF metadata including orientation
+        rotate: null, // Let Sharp auto-rotate based on EXIF
+      },
+    },
+  },
 });
